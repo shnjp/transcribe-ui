965cdf57600a4c6192c1dc80663ae74c860e8a51
MukIhara Ryohei <mukaihara@MukaiharanoMBP.lan>
2020-12-08 15:17:02 +0900

    フォームから送信できるようになった

--- src/fileUpLoad.tsx
+++ src/fileUpLoad.tsx
@@ -1,51 +1,28 @@
 import React from 'react'
 
-interface State {
-  file: File | null
-}
+const url =
+  'https://mojimoji-voices.s3.amazonaws.com/0000f1a9-0621-4597-903c-94027762e905?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASJLBJ3RGAFH76TQL%2F20201208%2Fap-northeast-1%2Fs3%2Faws4_request&X-Amz-Date=20201208T054837Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=81148e3cbdfd1a0c27617fed949659688fe5f63060052b40ec6444e841cdee18'
 
-class FileUpLoadForm extends React.Component<{}, State> {
-  constructor(props: {}) {
-    super(props)
-    this.setState({file: null})
-  }
-
+class FileUpLoadForm extends React.Component {
   private handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log('e.target.files', e.target.files)
     if (e.target.files == null) {
       console.log('file is null')
       return 0
     }
-    this.setState({file: e.target.files[0]})
-    const params = new FormData()
-    params.append('file', e.target.files[0])
-    console.log('params', params)
-    const url =
-      'https://mojimoji-voices.s3.amazonaws.com/a59892ee-b373-4a01-84c8-f68483bdb940?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASJLBJ3RGAFH76TQL%2F20201204%2Fap-northeast-1%2Fs3%2Faws4_request&X-Amz-Date=20201204T061409Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=ef38c288c76f05389a4212973bedd8d37198d7325b34f717b9c655d1bb2babae'
+
     const response = await fetch(url, {
-      method: 'POST',
-      body: params,
-      mode: 'no-cors',
+      method: 'PUT',
+      body: e.target.files[0],
     })
-      .then((response) => response.json())
-      .then((result) => {
-        console.log('Success:', result)
-      })
-      .catch((error) => {
-        console.error('Error:', error)
-      })
     console.log(response)
   }
 
   render() {
     return (
-      <form
-        action="https://mojimoji-voices.s3.amazonaws.com/a59892ee-b373-4a01-84c8-f68483bdb940?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASJLBJ3RGAFH76TQL%2F20201204%2Fap-northeast-1%2Fs3%2Faws4_request&X-Amz-Date=20201204T061409Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=ef38c288c76f05389a4212973bedd8d37198d7325b34f717b9c655d1bb2babae"
-        method="post"
-        encType="multipart/form-data"
-      >
+      <form>
         <h1>File Upload</h1>
-        <input type="file" onChange={this.handleChange} />
+        <input type="file" accept="video/mp4" onChange={this.handleChange} />
         <input type="submit" />
       </form>
     )
