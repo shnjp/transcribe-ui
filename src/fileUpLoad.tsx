1b36ff69276c1a969c994f15902574e64eaa92ac
MukIhara Ryohei <mukaihara@MukaiharanoMBP.lan>
2020-12-11 16:00:49 +0900

    アップロードボタンクリックでアップロードするように変更

--- src/fileUpLoad.tsx
+++ src/fileUpLoad.tsx
@@ -1,29 +1,62 @@
 import React from 'react'
 
-const url =
-  'https://mojimoji-voices.s3.amazonaws.com/0000f1a9-0621-4597-903c-94027762e905?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASJLBJ3RGAFH76TQL%2F20201208%2Fap-northeast-1%2Fs3%2Faws4_request&X-Amz-Date=20201208T054837Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=81148e3cbdfd1a0c27617fed949659688fe5f63060052b40ec6444e841cdee18'
+const endpoint = 'https://tzh11pi5sf.execute-api.ap-northeast-1.amazonaws.com/Prod/pre-signed'
 
-class FileUpLoadForm extends React.Component {
-  private handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('e.target.files', e.target.files)
-    if (e.target.files == null) {
+interface State {
+  uploadFile: null | File
+}
+
+class FileUpLoadForm extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props)
+    this.state = {uploadFile: null}
+  }
+
+  getPreSignedURL = async () => {
+    const url = await fetch(endpoint, {method: 'GET'})
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        return data['preSignedURL']
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+    return url
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files == null) {
       console.log('file is null')
-      return 0
+      return
     }
+    this.setState({uploadFile: event.target.files[0]})
+  }
 
+  handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log('event.target: ', event)
+    if (this.state.uploadFile == null) {
+      console.log('file is null')
+      return
+    }
+    const url = await this.getPreSignedURL()
+    console.log('presigned URL:', url)
     const response = await fetch(url, {
       method: 'PUT',
-      body: e.target.files[0],
+      body: this.state.uploadFile,
     })
     console.log(response)
+    return
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h1>File Upload</h1>
         <input type="file" accept="video/mp4" onChange={this.handleChange} />
-        <input type="submit" />
+        <input type="submit" value="アップロード" />
       </form>
     )
   }
